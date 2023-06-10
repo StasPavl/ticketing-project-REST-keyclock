eab7fb71df2943e0c1f3ed8b1145f847a9eb3222
stanislavpavlovskyi <lichfb@gmail.com>
2023-06-10 16:14:13 -0400

    UserController API end points created

---
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getUsers(){
+        List<UserDTO> listDTO = userService.listAllUsers();
+        return ResponseEntity.ok(
+                new ResponseWrapper("Users retrieved successful", listDTO, HttpStatus.OK));
+    }
+    @GetMapping("/{username}")
+    public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable String username){
+        UserDTO user = userService.findByUserName(username);
+        return ResponseEntity.ok(new ResponseWrapper("user found", user,HttpStatus.FOUND));
+
+    }
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO userDto){
+        userService.save(userDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User created", HttpStatus.OK));
+    }
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO userDTO){
+        userService.update(userDTO);
+        return ResponseEntity.ok(new ResponseWrapper("user updated", HttpStatus.OK));
+    }
+    @DeleteMapping("/{username}")
+    public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable String username){
+        userService.delete(username);
+        return ResponseEntity.ok(new ResponseWrapper("user deleted", HttpStatus.OK));
+    }
+}
